a1dbff403f2b37f28c441fb282edb1aae844febc
max77p <shanindrakumar@gmail.com>
2019-10-25 17:57:28 -0400

    update

--- tableViewCustomCellApp/ViewController.swift
+++ tableViewCustomCellApp/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
