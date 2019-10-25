383607dc607ae10a619ac3cb3c853cec725c9fca
max77p <shanindrakumar@gmail.com>
2019-10-25 18:16:06 -0400

    update

--- tableViewCustomCellApp/FoodTableViewCell.swift
+++ tableViewCustomCellApp/FoodTableViewCell.swift
@@ -13,6 +13,17 @@ class FoodTableViewCell: UITableViewCell {
     var nameLabel: UILabel?
     var caloriesLabel: UILabel?
     var iconImageView: UIImageView?
-
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier:String?){
+        super.init(style:style,reuseIdentifier:reuseIdentifier)
+    }
+    required init?(coder: NSCoder) {
+        //should implement this if you're going to use InterfaceBuilder to design your cell
+        fatalError("init(coder:) hass not been implemented")
+    }
+    
+    func setCell(name: String,calories:String, image:String){
+        
+    }
 }
 
