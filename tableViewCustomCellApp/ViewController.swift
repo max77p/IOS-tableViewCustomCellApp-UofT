4f51cf913acfb39ce244fc8933cdc1dab1397b00
max77p <shanindrakumar@gmail.com>
2019-10-25 18:00:25 -0400

    added delegate

--- tableViewCustomCellApp/ViewController.swift
+++ tableViewCustomCellApp/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+
+    
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.dataSource=self
+        self.tableView.delegate=self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 
 
