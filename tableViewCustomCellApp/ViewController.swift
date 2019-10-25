6ea4c55b8ed181db6dd7e3e1a0c23b2c912ad083
max77p <shanindrakumar@gmail.com>
2019-10-25 17:14:58 -0400

    Initial Commit

---
+++ tableViewCustomCellApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewCustomCellApp
+//
+//  Created by Shan Indrakumar on 2019-10-25.
+//  Copyright © 2019 CS2680. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
